5535dee027b0139fde480cb99d987e76cf79d465
Medikan <joshdubisz@gmail.com>
2015-10-14 20:38:47 -0700

    Initial Commit

---
+++ CS445_Project1_Client.java
@@ -0,0 +1,57 @@
+/* CS 445 - Computer Architecture & Organization
+ * File Name: CS445_Project1_Client.java
+ * Project 1 - Due 10/18/2015
+ * Instructor: Dr. Dan Grissom
+ * 
+ * Name: Josh Dubisz
+ * Name: Carson Hall
+ * Description: Insert your meaningful description for the BigEndianMemorySystem class.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+// INSTRUCTIONS: Update the header above with the correct due date,
+// first/last name(s) and description for this specific project. You should also update
+// the first/last name below in the "System.out.println" statement at the beginning
+// of the "main()" method. Failure to do so will result in lost points. DO NOT change
+// the name of the class or the autograder will give you 0 points.
+//
+// COLLABORATION: Students may work alone or with 1 (one) partner. AT NO POINT should you
+// ever look at the code of your classmate's project OR show your code to help another
+// classmate. If you need help, please ask your instructor, who is MORE THAN HAPPY to assist
+// you and point you in the right direction.
+//
+// CHEATING: Your projects will be automatically and algorithmically checked for cheating
+// by the online submission system. These algorithms are very good and will check against
+// each of your classmate's submissions (in this section and other sections), projects that
+// have been submitted in the past by previous students at this university and other
+// universities, as well as content and code found on the internet. In short, if you cheat,
+// EXPECT TO BE CAUGHT.
+//
+// Be aware that being caught for cheating can result in a 0% on the project for you AND the
+// person that gave their code (it is your responsibility to not share your code, too). By the
+// course breakdown, this will result in an automatic loss in 15% of your entire course grade.
+//
+// The instructor is available for help and there is NO REASON to take the risk in cheating.
+///////////////////////////////////////////////////////////////////////////////
+public class CS445_Project1_Client
+{
+	public static void main(String[] args)
+	{
+		// Your program should always output your name and the project number.
+		// DO NOT DELETE OR COMMENT OUT. Replace with relevant info.
+		System.out.println("Josh Dubisz & Carson Hall");
+		System.out.println("CS445 Project 1");
+		System.out.println("");
+
+		///////////////////////////////////////////////////////////////////////
+		// Your code should go below these comments.
+		//
+		// See the "User Interface Requirements - Details" section (with
+		// steps 1-4, including all sub-steps) of the project description
+		// for a description of the program flow that should be found here.
+		//
+		// When done, your console output should look similar to that seen in the
+		// "User Interface Requirements - Sample Console Input/Output" section of
+		// the project description.
+	}
+}
---
+++ LittleEndianMemorySystem.java
@@ -0,0 +1,77 @@
+/* CS 445 - Computer Architecture & Organization
+ * File Name: LittleEndianMemorySystem.java
+ * Project 1 - Due 10/18/2015
+ * Instructor: Dr. Dan Grissom
+ * 
+ * Name: Josh Dubisz
+ * Name: Carson Hall
+ * Description: Insert your meaningful description for the BigEndianMemorySystem class.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+// INSTRUCTIONS: Update the header above with the correct due date,
+// first/last name(s) and description for this specific project. You should also update
+// the first/last name below in the "System.out.println" statement at the beginning
+// of the "main()" method. Failure to do so will result in lost points. DO NOT change
+// the name of the class or the autograder will give you 0 points.
+//
+// COLLABORATION: Students may work alone or with 1 (one) partner. AT NO POINT should you
+// ever look at the code of your classmate's project OR show your code to help another
+// classmate. If you need help, please ask your instructor, who is MORE THAN HAPPY to assist
+// you and point you in the right direction.
+//
+// CHEATING: Your projects will be automatically and algorithmically checked for cheating
+// by the online submission system. These algorithms are very good and will check against
+// each of your classmate's submissions (in this section and other sections), projects that
+// have been submitted in the past by previous students at this university and other
+// universities, as well as content and code found on the internet. In short, if you cheat,
+// EXPECT TO BE CAUGHT.
+//
+// Be aware that being caught for cheating can result in a 0% on the project for you AND the
+// person that gave their code (it is your responsibility to not share your code, too). By the
+// course breakdown, this will result in an automatic loss in 15% of your entire course grade.
+//
+// The instructor is available for help and there is NO REASON to take the risk in cheating.
+///////////////////////////////////////////////////////////////////////////////
+
+public class LittleEndianMemorySystem
+{
+	///////////////////////////////////////////////////////////////////////
+	// YOU MAY ADD ANY ADDITIONAL METHODS OR MODIFY THE ONES BELOW. THE 
+	// METHOD TEMPLATES FOUND IN THIS FILE ARE JUST TO HELP YOU GET STARTED
+	// AND UNDERSTAND THE REQUIRMENTS DETAILED IN THE PROJECT DESCRIPTION.
+	
+	///////////////////////////////////////////////////////////////////////
+	// Constructor (you may modify this as you see fit)
+	LittleEndianMemorySystem()
+	{
+		
+	}
+
+	///////////////////////////////////////////////////////////////////////
+	// Method to receive raw data from a big endian system (HINT: You may
+	// want to take in a parameter of a 1D or 2D byte array.) You cannot 
+	// pass anything other than raw data (no helper variables). This method
+	// will receive the raw bytes and convert it to it's own system, overwriting
+	// it's internal memory.
+	public void receiveRawBytesFromBigEndianMem(???)
+	{
+		
+	}
+	
+	///////////////////////////////////////////////////////////////////////
+	// Gets a copy of the raw bytes (should be instance variable) and returns
+	// it to caller. See above comment for hint on what data type would be best
+	public ??? getRawBytes()
+	{
+		
+	}
+	
+	///////////////////////////////////////////////////////////////////////
+	// Prints the raw data to the console as described in the project
+	// description
+	public void printRawBytes()
+	{
+		
+	}
+}
